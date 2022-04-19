200967698a2cc3dca00e5376f89b7ed3bd901270
PraiseAyinla <99278113+PraiseAyinla@users.noreply.github.com>
2022-04-19 16:25:45 +0100

    Update write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -16,4 +16,27 @@ int _putchar(char c)
 
 	if (c == -1 || i >= 1024)
 	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
 
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
